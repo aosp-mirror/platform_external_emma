55242f8cedef60df689a8ad9015b8d7a74bbd1fe
Jean-Baptiste Queru <jbq@google.com>
2009-11-12 18:45:25 -0800

    eclair snapshot

--- Android.mk
+++ Android.mk
@@ -1,6 +1,10 @@
 # Copyright 2008 The Android Open Source Project
 
 LOCAL_PATH := $(my-dir)
+
+
+# the custom dex'ed emma library ready to put on a device.
+# ============================================================
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, core)
@@ -14,3 +18,17 @@ LOCAL_JAVA_RESOURCE_DIRS := core/res
 LOCAL_NO_EMMA_INSTRUMENT := true
 
 include $(BUILD_JAVA_LIBRARY)
+
+
+
+# the custom emma library to add to an SDK project.
+# ============================================================
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(call all-java-files-under, core)
+
+LOCAL_MODULE := emmalib
+
+LOCAL_JAVA_RESOURCE_DIRS := core/res
+
+include $(BUILD_HOST_JAVA_LIBRARY)
